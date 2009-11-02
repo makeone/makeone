91491f3b5a54363ee8d1cf558ac5cbb81a08c772
James A Barkley <jbarkley@ambrosia.site>
2009-11-02 18:55:01 -0500

    - initial commit of existing project files and default rails deploy

---
+++ trunk/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_trunk_session',
+  :secret      => '2ad1decde3fca8f0531ee662adeca3d6e8b232e5fc1db6fa5707def6a21405851ed68b757ff9a51d6484078929a128609d190c7ee9926f247540af2a712798ce'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
